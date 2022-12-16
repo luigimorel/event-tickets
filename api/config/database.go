c2cff96a4f3e07b613c9c58bab9c367e888a89cf
Luigi Morel <morelmiles@gmail.com>
2022-12-16 22:50:07 +0300

    database config

---
+++ api/config/database.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/morelmiles/go-events/internals/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	DB *gorm.DB
+)
+
+func Config() {
+	err := godotenv.Load("../.env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	username := os.Getenv("DB_USERNAME")
+	password := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+
+	dbLink := fmt.Sprintf("host=%s user=%s dbname=%s port=%s sslmode=disable TimeZone=Africa/Nairobi password=%s",
+		dbHost, username, dbName, dbPort, password)
+	DB, err = gorm.Open(postgres.Open(dbLink), &gorm.Config{})
+
+	if err != nil {
+		log.Println(err)
+	} else {
+		fmt.Println("Connected to the database!")
+	}
+
+	DB.Debug().AutoMigrate(
+		&models.Event{},
+		&models.User{},
+	)
+}
