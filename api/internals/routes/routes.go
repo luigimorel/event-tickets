66a1722ed620933fceeeb26d5aab410d4f17f21d
Luigi Morel <morelmiles@gmail.com>
2022-12-31 13:45:08 +0300

    add auth routes

--- api/internals/routes/routes.go
+++ api/internals/routes/routes.go
@@ -19,26 +19,32 @@ func Routes() {
 
 	router := mux.NewRouter().StrictSlash(true)
 	corsHandler := cors.Default().Handler(router)
+
+	// Swggers
 	router.PathPrefix("/api/v1/swagger/").Handler(httpSwagger.WrapHandler)
 
 	// Home
-	router.HandleFunc("/", controllers.Home).Methods("GET")
+	router.HandleFunc("/", helpers.SetMiddlewareJSON(controllers.Home)).Methods("GET")
+
+	// Auth
+	router.HandleFunc("/api/v1/auth/login", helpers.SetMiddlewareJSON(controllers.Login)).Methods("POST")
 
 	// Users
-	router.HandleFunc("/api/v1/users", controllers.GetUsers).Methods("GET")
-	router.HandleFunc("/api/v1/user/{id}", controllers.GetUserById).Methods("GET")
-	router.HandleFunc("/api/v1/register", controllers.CreateUser).Methods("POST")
-	router.HandleFunc("/api/v1/user/{id}", controllers.DeleteUserById).Methods("DELETE")
-	router.HandleFunc("/api/v1/user/{id}", controllers.UpdateUserById).Methods("PATCH")
-	router.HandleFunc("/api/v1/user/events/{id}", controllers.GetAllEventsByUser).Methods("GET")
+	router.HandleFunc("/api/v1/users", helpers.SetMiddlewareAuthentication(controllers.GetUsers)).Methods("GET")
+	router.HandleFunc("/api/v1/user/{id}", helpers.SetMiddlewareJSON(controllers.GetUserById)).Methods("GET")
+	router.HandleFunc("/api/v1/register", helpers.SetMiddlewareJSON(controllers.CreateUser)).Methods("POST")
+	router.HandleFunc("/api/v1/user/{id}", helpers.SetMiddlewareAuthentication(controllers.DeleteUserById)).Methods("DELETE")
+	router.HandleFunc("/api/v1/user/{id}", helpers.SetMiddlewareAuthentication(controllers.UpdateUserById)).Methods("PATCH")
+	router.HandleFunc("/api/v1/user/events/{id}", helpers.SetMiddlewareJSON(controllers.GetAllEventsByUser)).Methods("GET")
 
 	// Events
-	router.HandleFunc("/api/v1/events", controllers.GetEvents).Methods("GET")
-	router.HandleFunc("/api/v1/homepage_events", controllers.GetHomePageEvents).Methods("GET")
-	router.HandleFunc("/api/v1/events/{id}", controllers.GetEventById).Methods("GET")
-	router.HandleFunc("/api/v1/create", controllers.CreateEvent).Methods("POST")
-	router.HandleFunc("/api/v1/events/{id}", controllers.DeleteEventById).Methods("DELETE")
-	router.HandleFunc("/api/v1/events/{id}", controllers.UpdateEventById).Methods("PUT")
+	router.HandleFunc("/api/v1/events", helpers.SetMiddlewareJSON(controllers.GetEvents)).Methods("GET")
+	router.HandleFunc("/api/v1/homepage_events", helpers.SetMiddlewareJSON(controllers.GetHomePageEvents)).Methods("GET")
+	router.HandleFunc("/api/v1/events/{id}", helpers.SetMiddlewareJSON(controllers.GetEventById)).Methods("GET")
+	router.HandleFunc("/api/v1/create", helpers.SetMiddlewareAuthentication(controllers.CreateEvent)).Methods("POST")
+	router.HandleFunc("/api/v1/events/{id}", helpers.SetMiddlewareAuthentication(controllers.DeleteEventById)).Methods("DELETE")
+	router.HandleFunc("/api/v1/events/{id}", helpers.SetMiddlewareAuthentication(controllers.UpdateEventById)).Methods("PUT")
 
+	// Server port
 	http.ListenAndServe(":"+port, corsHandler)
 }
